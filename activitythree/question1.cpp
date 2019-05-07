46f8c48a8738141158438a97abaf84b9fd971c0b
fernandomaxx <32984809+fernandomaxx@users.noreply.github.com>
2019-05-07 04:20:50 -0300

    Update question1.cpp

--- activitythree/question1.cpp
+++ activitythree/question1.cpp
@@ -1,8 +1,10 @@
 /*
-Polimorfismo é difinido como um princípio a partir do qual as classes derivadas de uma única classe base são capazes de invocar os métodos que, embora apresentem a mesma assinatura, comportam-se de maneira diferente para cada uma das classes derivadas.
+Polimorfismo é difinido como um princípio a partir do qual as classes derivadas de uma única classe base são capazes de 
+invocar os métodos que, embora apresentem a mesma assinatura, comportam-se de maneira diferente para cada uma das classes 
+derivadas.
 
-Um exemplo disso seria uma classe base abstrata animal que possui o método abstrato
-emitirSom, para as diferentes classes derivadas esse método emitir som irá ser implementado de maneira diferente.
+Um exemplo disso seria uma classe base abstrata animal que possui o método abstrato emitirSom, para as diferentes classes 
+derivadas esse método emitir som irá ser implementado de maneira diferente.
 */
 //Exemplo:
 
@@ -37,7 +39,11 @@ class Abelha : public Animal
 }
 
 /* 
-Uma das vantagens de usar o polimorfismo é a possibilidade de que a partir de uma referência para classe base conseguir ivocar os métodos das instancias das classes derivadas que foram declarados como virtuais sem necessariamente ter uma referência para classe derivada ou fazer um downcast. Desta forma podendo até ter uma array de ponteiros para a classe base referenciando as instancias das classes derivadas e eles iriam responder conforme sua implementação graças ao polimorfismo.
+Uma das vantagens de usar o polimorfismo é a possibilidade de que a partir de uma referência para classe base conseguir 
+ivocar os métodos das instancias das classes derivadas que foram declarados como virtuais sem necessariamente ter uma 
+referência para classe derivada ou fazer um downcast. Desta forma podendo até ter uma array de ponteiros para a classe 
+base referenciando as instancias das classes derivadas e eles iriam responder conforme sua implementação graças ao 
+polimorfismo.
 */
 //Exemplo:
 
